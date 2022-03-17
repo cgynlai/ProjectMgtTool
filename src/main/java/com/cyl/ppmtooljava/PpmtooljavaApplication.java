200fb39f9e632d0fa97079b6d4eff49435c43931
cgynlai <cgynlai@gmail.com>
2022-03-17 18:59:13 +0800

    first src commit

---
+++ src/main/java/com/cyl/ppmtooljava/PpmtooljavaApplication.java
@@ -0,0 +1,13 @@
+package com.cyl.ppmtooljava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PpmtooljavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PpmtooljavaApplication.class, args);
+    }
+
+}
