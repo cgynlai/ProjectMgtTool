6109554d76efc1970c196a5cfb46f11849e19b81
cgynlai <cgynlai@gmail.com>
2022-03-20 15:16:50 +0800

    complete CRUD

--- src/main/java/com/cyl/ppmtooljava/repositories/ProjectRepository.java
+++ src/main/java/com/cyl/ppmtooljava/repositories/ProjectRepository.java
@@ -6,6 +6,17 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ProjectRepository extends CrudRepository<Project, Long> {
-    @Override
-    Iterable<Project> findAllById(Iterable<Long> longs);
+//    @Override
+//    Iterable<Project> findAllById(Iterable<Long> longs);
+
+    Project findByProjectIdentifier(String projectId);
+
+//    @Override
+//    Iterable<Project> findAll();
+
+
+//    @Override
+//    void delete(Project entity);
+
+
 }
--- src/main/java/com/cyl/ppmtooljava/services/ProjectService.java
+++ src/main/java/com/cyl/ppmtooljava/services/ProjectService.java
@@ -24,4 +24,30 @@ public class ProjectService {
 
     }
 
+    public Project findProjectByIdentifier(String projectId){
+
+        Project project = projectRepository.findByProjectIdentifier(projectId.toUpperCase());
+
+        if(project==null){
+            throw new ProjectIdException("Project ID '" + projectId + "' does not exist");
+        }
+
+        return project;
+
+
+    }
+
+    public Iterable<Project> findAllProject(){
+        return projectRepository.findAll();
+    }
+
+    public void deleteProjectByIdentifier(String projectId){
+        Project project = projectRepository.findByProjectIdentifier(projectId);
+
+        if(project==null){
+            throw new ProjectIdException("can't find project with ID '" + projectId + "'. This project does not exist");
+
+        }
+        projectRepository.delete(project);
+    }
 }
